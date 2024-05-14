483359e966e7bed16e4d05c0bdbd1aa2d01e097c
Rodrigo <rodrigopractic@gmail.com>
2024-05-14 00:52:57 -0300

    Subida de JS y Contacto, añadido el href / en los demas

---
+++ static/js/validacion.js
@@ -0,0 +1,47 @@
+// var imagenes = ["1.jpg", "2.jpg","3.jpg", "4.jpg", "5.jpg", 
+// "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"
+//     ]
+// var indice = 0
+
+// function cambiarImagen(){
+//     document.getElementById("imagen").src = imagenes[indice];
+//     indice++;
+//     if (indice >= imagenes.length){
+//         indice = 0;
+//     }
+// }
+
+function validarFormulario() {
+    // Entrada
+    var nombre = document.getElementById("firstname").value;
+    var email = document.getElementById("email").value;
+    var checkboxes = document.querySelectorAll('input[name="asuntos"]:checked');
+  
+    // Validar que los campos no estén vacíos
+    if (nombre.trim() === '' || email.trim() === '' || checkboxes.length === 0) {
+      alert("Por favor, completa todos los campos.");
+      return false; // Evita que el formulario se envíe
+    } else {
+      return true;
+    }
+}
+
+//   Imagenes caja contacto
+document.addEventListener('DOMContentLoaded', function() {
+    var cajaContacto = document.getElementById('cajaContacto');
+    var imagenes = ['/static/img/Contacto/1.jpg', '/static/img/Contacto/2.jpg', '/static/img/Contacto/3.jpg', '/static/img/Contacto/4.jpg']; // Lista de imágenes a mostrar
+    var tiempoEntreCambio = 3000; // Tiempo en milisegundos entre cada cambio 
+    var indice = 0;
+
+    // Función para cambiar la imagen
+    function cambiarImagen() {
+        cajaContacto.style.backgroundImage = 'url(' + imagenes[indice] + ')';
+        indice = (indice + 1) % imagenes.length; // Avanza al siguiente índice, volviendo al inicio cuando se alcanza el final
+    }
+
+    // Llama a la función cambiarImagen inicialmente
+    cambiarImagen();
+
+    // Establece un intervalo para cambiar la imagen cada cierto tiempo
+    setInterval(cambiarImagen, tiempoEntreCambio);
+});
